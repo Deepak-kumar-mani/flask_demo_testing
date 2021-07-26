b2c08d9ccac0dcafffdf364204bc9737697cf222
Deepak-kumar-mani <r.m.theepakkumar@gmail.com>
2021-07-26 09:17:33 +0530

    comment update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    return "<h1> this is flas application </h1>"
+    return "<h1> Hi Jo this my first application deployed in cloud environment </h1>"
 
 if __name__ == "__main__":
     app.run()
